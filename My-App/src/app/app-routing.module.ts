ef9df9334dfe7a79272e3f28a6f4811703a2760e
victor-oli1999 <88786542+victor-oli1999@users.noreply.github.com>
2023-06-02 11:57:22 -0300

    Conclusão da parte 5

--- My-App/src/app/app-routing.module.ts
+++ My-App/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+/* Em baixo é onde mostra que tela mostrar dependendo da URL  */
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  /*O : indica que é um placeholder a um determinado hero id.*/
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- My-App/src/app/hero-detail/hero-detail.component.ts
+++ My-App/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { Hero } from '../heroes/hero'
+import { Hero } from '../heroes/hero';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,4 +13,23 @@ import { Hero } from '../heroes/hero'
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+  
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
